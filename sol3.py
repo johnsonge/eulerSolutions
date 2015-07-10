88169f971958068b38ae2048d69032dac8aa0c29
johnsonge <johnsonge@g.cofc.edu>
2015-07-09 20:26:51 -0400

    Completed problem 3

--- sol3.py
+++ sol3.py
@@ -1,6 +1,14 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143?
 
-n = 13195
-for i in range (1,n)
-    if i % n == 1 and i % n ==
+n = 600851475143
+prime = []
+
+#Finding Primes
+#Stop searching for primes after sqrt(n) is reached (only odd)
+i = 2
+for i in range(2, int(n**0.5)):
+	if n % i == 0:
+		n = n / i
+		prime.append(i)
+print prime[-1]
